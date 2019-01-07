4fbad17952b23ce65a30bcde994e258278f8c170
Javier Gonzalez Llamas <lastoner4@gmail.com>
2019-01-07 22:47:39 +0000

    Added zones

--- Classes/Professions/KindOfProfessions/Woodcuter.php
+++ Classes/Professions/KindOfProfessions/Woodcuter.php
@@ -4,5 +4,15 @@ require (dirname(__DIR__).'/MainProfession.php');
 class woodcuter extends profession
 {
   public $type = "Woodcuter";
-  public $resource = "Wood";
+  public $generatedresource = "Wood";
+
+  function NewWoodcuter($beingid, $villageid, $zoneId)
+  {
+    $this->beingId = $beingid;
+    $this->villageId = $villageid;
+    $this->zoneId = $zoneId;
+    $this->moneyperhour = rand(8,10);
+    $this->schedule = rand(1,3);
+    $this->amountperhour = rand(80,100);
+  }
 }
--- try.php
+++ try.php
@@ -5,46 +5,48 @@ require "Functions/BbddFunctions/Update.php";
 require "Classes/Beings/KindOfBeings/Human.php";
 require "Classes/Buildings/KindOfBuilding/Shack.php";
 require "Classes/Villages/KindOfVillages/Romanicvillage.php";
-require "Classes/Professions/MainProfession.php";
-
-/*
-$profession = new profession;
-$profession->beingId = 1;
-$profession->type = "Woodcuter";
-$profession->villageId = 1;
-$profession->moneyperhour = 10;
-$profession->fatigueperhour = 5;
-$profession->schedule = 1;
-$profession->resource = "Wood";
-$profession->amountperhour = 5;
-$profession->consumedresource = "NULL";
-$profession->consumedamount = 0;
-$villageid = NewInsert("professions", $profession);
-*/
+require "Classes/Professions/KindOfProfessions/Woodcuter.php";
+require "Classes/Zones/KindOfZones/Forest.php";
 
 $i = 0;
 $x = 50;
-while($i < 1000)
+$z = 0;
+while($i < 100)
 {
-if($x == 50)
-{
-$village = new romanicvillage();
-$village->NewVillage();
-$villageid = NewInsert("villages", $village);
-$x = 0;
-}
-// $poblation = count(Select("buildings", "buildingId", "villageId=$villageid"));
+  if($x == 50)
+  {
+    $village = new romanicvillage();
+    $village->NewVillage();
+    $villageid = NewInsert("villages", $village);
 
-$human = new human();
-$human->NewHuman();
-$beingid = NewInsert("beings", $human);
+    while($z < 5)
+    {
+      $zone = new forest();
+      $zone->name = $i . '-' . $z . '- zonetest';
+      $zone->resorceamount = rand(100,10000);
+      $zoneid[$z] = NewInsert("zones", $zone);
+      $z++;
+    }
+    $x = 0;
+    $z = 0;
+  }
+  // $poblation = count(Select("buildings", "buildingId", "villageId=$villageid"));
 
-$house = new shack();
-$house->NewShack($beingid);
-$house->villageId = $villageid;
-$buildingid = NewInsert("buildings", $house);
-Update("beings", "buildingId='$buildingid'", "beingId='$beingid'");
+  $human = new human();
+  $human->NewHuman();
+  $beingid = NewInsert("beings", $human);
 
-$x++;
-$i++;
+  $profession = new woodcuter;
+  $q = rand(0,4);
+  $profession->NewWoodcuter($beingid, $villageid, $zoneid[$q]);
+  $professionid = NewInsert("professions", $profession);
+
+  $house = new shack();
+  $house->NewShack($beingid);
+  $house->villageId = $villageid;
+  $buildingid = NewInsert("buildings", $house);
+  Update("beings", "buildingId='$buildingid'", "beingId='$beingid'");
+
+  $x++;
+  $i++;
 }
