cc11d8b84ae2dc74a3d160b00e0c83cc2fdf4370
Javier Gonzalez Llamas <lastoner4@gmail.com>
2019-01-13 19:38:37 +0000

    Added stamina and storages

--- Functions/EngineFunctions/SubEngineProcess/Working.php
+++ Functions/EngineFunctions/SubEngineProcess/Working.php
@@ -4,9 +4,11 @@ function Working($people)
   foreach ($people as $person)
   {
      $skill = SelectOne("beings", $person['requieredskill'], "beingId=$person[beingId]");
+     $stamina = SelectOne("beings", "stamina", "beingId=$person[beingId]");
 
      //by now the wood cost 5, this is only for test
-     $resourceamount = ($skill * 0.2) * 8;
+
+     $resourceamount = (($skill * 0.2) + ($stamina * 0.1)) * 8;
      $earnmoney = $resourceamount/6;
 
      $currentmoney = SelectOne("beings", "money", "beingId=$person[beingId]");
@@ -18,6 +20,10 @@ function Working($people)
      $currentmoney = SelectOne("villages", "money", "villageId=$villageid");
      $totalmoney = $currentmoney - $earnmoney;
      Update("villages", "money=$totalmoney", "villageId=$villageid");
+
+     $currentresource = SelectOne("storages", "$person[generatedresource]", "ownerId=$villageid");
+     $totalresource = $currentresource + $resourceamount;
+     Update("storages", "$person[generatedresource]=$totalresource", "ownerId=$villageid");
   }
 
 }
