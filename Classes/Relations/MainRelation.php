a5ce93fb1ecead8c8f33ee16d6df15c62708fda3
Javier Gonzalez Llamas <lastoner4@gmail.com>
2019-02-11 20:22:26 +0000

    Added specific select to relations

--- Classes/Relations/MainRelation.php
+++ Classes/Relations/MainRelation.php
@@ -3,6 +3,7 @@ require_once (dirname(__DIR__).'/App.php');
 
 class relation extends app
 {
+ public $relationId;
  public $beingId;
  public $relationtypeId;
  public $reciverId;
@@ -10,4 +11,25 @@ class relation extends app
 
  public $table = "relations";
  public $relations = ["beings"];
+
+ public function SelectRelation($beingid, $reciverId)
+ {
+   require "Config/bbdd.php";
+
+   $sql = "SELECT * FROM $this->table WHERE beingId = $beingid AND reciverId = $reciverId";
+
+   $result = $conn->query($sql)->fetch_object();
+   unset($sql);
+   $conn->close();
+
+   if(!empty($result))
+   {
+     $vars = get_object_vars($result);
+
+     foreach ($vars as $name => $oldvalue)
+     {
+       $this->$name = isset($vars[$name]) ? $vars[$name] : NULL;
+     }
+   }
+ }
 }
