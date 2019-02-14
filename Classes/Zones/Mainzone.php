31a9883e7fe6ceccebf883a686021607b730af3f
Javier Gonzalez Llamas <lastoner4@gmail.com>
2019-02-14 18:57:24 +0000

    Added function endzone

--- Classes/Zones/Mainzone.php
+++ Classes/Zones/Mainzone.php
@@ -11,4 +11,15 @@ class zone extends app
  public $resourceamount;
 
  public $table = "zones";
+
+ public function EndZone()
+ {
+   require "Functions/BbddFunctions/Manual.php";
+
+   Manual("UPDATE beings JOIN professions ON beings.beingId = professions.beingId
+   SET beings.professionId=0 WHERE zoneId = $this->zoneId");
+   
+   Manual("DELETE FROM professions WHERE zoneId = $this->zoneId");
+   Manual("DELETE FROM zones WHERE zoneId = $this->zoneId");
+ }
 }
