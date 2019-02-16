2eea2c37cb95f5c6e305690b0c2806ee146c64f2
Javier Gonzalez Llamas <lastoner4@gmail.com>
2019-02-16 23:21:50 +0000

    Now test working normal

--- Test.php
+++ Test.php
@@ -1,11 +1,6 @@
 <?php
-require_once "Classes/Beings/KindOfBeings/Human.php";
-
-
-
-$human = new human;
-// $human->NewHuman();
-// $human->Insert();
-$human->SelectById(2);
-$human->name = "Flori";
-$human->Update();
+require "Functions/EngineFunctions/Year.php";
+while (TRUE)
+{
+Year();
+}
