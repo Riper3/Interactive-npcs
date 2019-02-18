65241ca858908a909426d54b7a3402375f97f65e
Javier Gonzalez Llamas <lastoner4@gmail.com>
2019-02-18 00:16:12 +0000

    Changed getprofession

--- Functions/CommonFunctions/GetBestProfession.php
+++ Functions/CommonFunctions/GetBestProfession.php
@@ -5,12 +5,12 @@ function GetBestProfession($human)
   $skills = ["strength" => $human->strength, "intelligence" => $human->intelligence, "dexterity" => $human->dexterity];
   if(!empty($avaliablezones))
   {
-    for ($i=0; $i=1;)
+    for ($i=0; $i<1;)
     {
       $bestskill = array_search(max($skills),$skills);
       $professionnew = SelectOne("professionstype", "name", "skill = '$bestskill'");
       $professiosource = SelectOne("professionstype", "resource", "skill = '$bestskill'");
-
+      
       if (in_array($professiosource, array_column($avaliablezones, "resource")))
       {
         $profession = new $professionnew;
@@ -18,9 +18,11 @@ function GetBestProfession($human)
         $newclass = "New".$professionnew;
         $profession->$newclass($human->villageId, $avaliablezones[$q]["zoneId"], $human->beingId);
         $profession->Insert();
-        $i = 1;
 
-        return $profession->professionId;
+        $human->professionId = $profession->professionId;
+        $human->Update();
+
+        $i = 1;
       }
       else
       {
