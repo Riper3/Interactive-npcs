fbb96f1ee34c23dd2d33aca8f5baf53bbf148aab
Javier Gonzalez Llamas <lastoner4@gmail.com>
2019-02-21 21:39:45 +0000

    Added autoloader

---
+++ Config/autoloader.php
@@ -0,0 +1,26 @@
+<?php
+
+spl_autoload_register(function ($class) {
+    $file = ucwords($class).'.php';
+    $allclass = scandir("Classes");
+    unset($allclass[0], $allclass[1], $allclass[2]);
+    foreach ($allclass as $oneclass)
+    {
+      $firstfolder = scandir("Classes/".$oneclass);
+      unset($firstfolder[0], $firstfolder[1]);
+      $checkfolders = strpos($firstfolder[2], '.php');
+      if(in_array($file, $firstfolder))
+      {
+        require "Classes/".$oneclass."/".$file;
+      }
+      elseif($checkfolders === false)
+      {
+        $secondfolder = scandir("Classes/".$oneclass."/".$firstfolder[2]);
+        unset($secondfolder[0], $secondfolder[1]);
+        if(in_array($file, $secondfolder))
+        {
+          require "Classes/Zones/KindOfZones/Forest.php";
+        }
+      }
+    }
+});
