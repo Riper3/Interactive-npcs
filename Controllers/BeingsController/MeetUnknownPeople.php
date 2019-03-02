63e85e6bff9abfcbaad529dc75a502f1c2a3d042
Javier Gonzalez Llamas <lastoner4@gmail.com>
2019-03-02 22:08:58 +0000

    Deleted print_r

--- Controllers/BeingsController/MeetUnknownPeople.php
+++ Controllers/BeingsController/MeetUnknownPeople.php
@@ -3,6 +3,5 @@ function MeetUnknownPeople($beingid, $reciverid)
 {
   $newrelation = new friend;
   $newrelation->NewFriend($beingid, $reciverid);
-  print_r($newrelation);
   $newrelation->Insert();
 }
